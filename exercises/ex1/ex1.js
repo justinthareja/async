c2769472c1d9898d07583881dd4d3f0d8e9c9a98
Justin Thareja <justin.thareja@gmail.com>
2021-02-05 12:03:01 -0500

    added initial solution for exercise 1

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -19,7 +19,52 @@ function output(text) {
 
 // **************************************
 // The old-n-busted callback way
+let files = {};
 
+function getFile(file) {
+	fakeAjax(file,function(text){
+		files[file] = {
+			text,
+			hasRendered: false
+		};
+
+		output(`Received: ${file}`);
+
+		if (file == "file1") {
+			output(text);
+			files[file].hasRendered = true;
+
+			if (files["file2"]) {
+				output(files["file2"].text);
+				files["file2"].hasRendered = true;
+
+				if (files["file3"]) {
+					output(files["file3"].text);
+					files["file3"].hasRendered = true;
+				}
+			}
+		} else if (file == "file2") {
+
+			if (files["file1"] && files["file1"].hasRendered) {
+				output(files["file2"].text);
+				files["file2"].hasRendered = true;
+
+				if (files["file3"]) {
+					output(files["file3"].text);
+					files["file3"].hasRendered = true;
+				}
+			} else {
+				output(`Previous file not loaded... storing ${file}`);
+			}
+		} else { // file3
+			if (files["file2"] && files["file2"].hasRendered) {
+				output(files["file3"].text);
+				files["file3"].hasRendered = true;
+			} else {
+				output(`Previous file not loaded... storing ${file}`);
+			}
+		}
+	});
 }
 
 // request all files at once in "parallel"
