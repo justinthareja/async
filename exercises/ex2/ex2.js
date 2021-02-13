051964c030fd69a56e3433a6f842e9de9f843044
Justin Thareja <justin.thareja@gmail.com>
2021-02-13 14:21:36 -0500

    added ex2 solution

--- exercises/ex2/ex2.js
+++ exercises/ex2/ex2.js
@@ -21,7 +21,42 @@ function output(text) {
 
 function getFile(file) {
 	// what do we do here?
+	// thunk constructor
+	// active thunk so does the async work on creation
+	var data;
+	var callback;
+
+	fakeAjax(file, function(result) {
+		if (callback) {
+			callback(result);
+		} else {
+			data = result;
+		}
+	});
+
+	return function(cb) {
+		if (data) {
+			cb(data)
+		} else {
+			callback = cb;
+		}
+	}
 }
 
+
 // request all files at once in "parallel"
-// ???
+var getFile1 = getFile("file1");
+var getFile2 = getFile("file2");
+var getFile3 = getFile("file3");
+
+// log the output in the correct order
+getFile1(function(file1) {
+	output(file1);
+	getFile2(function(file2) {
+		output(file2);
+		getFile3(function(file3) {
+			output(file3);
+			output("done");
+		});
+	});
+});
