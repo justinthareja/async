951ab3e6aae6ca5cb7130cda4fff95f0b8b01e68
Justin Thareja <justin.thareja@gmail.com>
2021-02-15 15:33:41 -0500

    added solution to ex3

--- exercises/ex3/ex3.js
+++ exercises/ex3/ex3.js
@@ -21,7 +21,29 @@ function output(text) {
 
 function getFile(file) {
 	// what do we do here?
+	return new Promise(function (resolve, reject) {
+		fakeAjax(file, function (data) {
+			resolve(data);
+		});
+	});
 }
 
 // request all files at once in "parallel"
 // ???
+var p1 = getFile("file1");
+var p2 = getFile("file2");
+var p3 = getFile("file3");
+
+p1.then(function (file1) {
+	output(file1);
+	return p2;
+})
+.then(function (file2) {
+	output(file2);
+	return p3;
+})
+.then(function (file3) {
+	output(file3);
+	output("Complete")
+})
+.catch(output)
